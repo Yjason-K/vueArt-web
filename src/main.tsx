a988129ca35ab77c4bd282bf68fa23c9a81c4646
김영재 <gomgom0324@gmail.com>
2025-04-20 21:34:31 +0900

    feat:  React Query 전역 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './App.tsx';
 import theme from '@assets/theme';
+import { queryClient } from '@utils/queryClient/queyClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* 기본 restCSS - true*/}
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      {/* 기본 restCSS - true*/}
+      <ChakraProvider theme={theme}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
