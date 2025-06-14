748677f860cff3f3e4e4bd4af3a15c02735f7e60
김영재 <gomgom0324@gmail.com>
2025-06-14 14:33:17 +0900

    chore: tsconfig alis 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@api': resolve(__dirname, './src/api'),
+    },
   },
 });
